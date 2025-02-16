bbbe38aaed5664eb535b6241594d1550b851f52f
wagnerrodrigo <wagnerrodrigo.pan@gmail.com>
2025-02-15 23:11:38 -0300

    Kata syntax fudamentals

--- Part 1 Syntax Fundamentals/Kata 1- Hello World.go
+++ Part 1 Syntax Fundamentals/Kata 1- Hello World.go
@@ -5,3 +5,227 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World")
 }
+
+// A partir desse ponto começa o Kata 
+
+package main 
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World")
+}
+
+
+package main 
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World")
+}
+
+
+package main 
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World")
+}
+
+
+package main 
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World")
+}
+
+
+package main 
+
+import "fmt"
+
+func main(){
+	fmt.Println("Hello, World")
+}
+
+
+package main 
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World")
+}
+
+
+package main 
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World")
+}
+
+
+package main 
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Hello, World")
+}
+
+
+package main 
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World")
+}
+
+package main 
+
+import "fmt"
+
+func main(){
+	fmt.Println("Hello, World")
+}
+
+
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Hello, World")
+}
+
+
+
+package main 
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World")
+}
+
+
+package main 
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World")
+}
+
+
+package main 
+
+import "fmt"
+
+func main(){
+	fmt.Println("Hello, World")
+}
+
+
+package main 
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World")
+}
+
+
+package main 
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World")
+}
+
+
+package main 
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World")
+}
+
+
+package main 
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World")
+}
+
+
+package main 
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World")
+}
+
+package main 
+
+import "fmt"
+
+func main() {
+	fmt.Prinln("Hello, World")
+}
+
+
+package main 
+
+import  "fmt"
+
+func main() {
+	fmt.Println("Hello, World")
+}
+
+
+package main 
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World")
+}
+
+
+package main 
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World")
+}
+
+package main 
+
+import "fmt"
+
+func main() {
+	fmt.Prinlnt("Hello, World")
+}
+
+
+package main 
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World")
+}
