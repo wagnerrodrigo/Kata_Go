c752c7d84f3fdb515680c615bd401a7c489ba7b6
wagnerrodrigo <wagnerrodrigo.pan@gmail.com>
2025-02-17 20:28:17 -0300

    Organizando pasta e fazendo segundo Kata sum of two numbers

---
+++ Part 1/Sum of two numbers/main.go
@@ -0,0 +1,273 @@
+//  Write a program that takes two intefres as input and priints their sum.
+
+package main
+
+import "fmt"
+
+func main() {
+	var a, b int
+	fmt.Println("Entre two numbers:")
+	fmt.Scan(&a, &b)
+	fmt.Println("Sum", a+b)
+}
+
+// a partir de agora vamos fazer o kata! :D
+
+package main
+
+import "fmt"
+
+func main() {
+	var a, b int
+	fmt.Println("Entre two numbers:")
+	fmt.Scan(&a, &b)
+	fmt.Println("Sum",a+b )
+}
+
+package main
+
+import "fmt"
+
+func main() {
+	var a, b int
+	fmt.Println("Entre two numbers:")
+	fmt.Scan(&a, &b)
+	fmt.Println("Sum", a+b)
+}
+
+package main
+
+import "fmt"
+
+func main() {
+	var a, b int
+	fmt.Println("Entre two numbers:")
+	fmt.Scan(&a, &b)
+	fmt.Println("Sum", a+b)
+}
+
+package main
+
+import "fmt"
+
+func main() {
+	var a, b int
+	fmt.Println("Entre two numbers:")
+	fmt.Scan(&a, &b)
+	fmt.Println("Sum",a+b)
+}
+
+package main
+
+import "fmt"
+
+func main() {
+	var a, b int
+	fmt.Println("Entre two numbers:")
+	fmt.Scan(&a, &b)
+	fmt.Println("Sum",a+b)
+}
+
+package main
+
+import "fmt"
+
+func main() {
+	var a, b int
+	fmt.Println("Entre two numbers:")
+	fmt.Scan(&a, &b)
+	fmt.Println("Sum", a+b)
+}
+
+
+package main 
+
+import "fmt"
+
+func main() {
+	var a, b int
+	fmt.Println("Entre two numbers:")
+	fmt.Scan(&a, &b)
+	fmt.Println("Sum",a+b)
+}
+
+
+package maiin 
+
+import "fmt"
+
+func main() {
+	var a, b int
+	fmt.Println("Entre two numbers:")
+	fmt.Scan(&a, &b)
+	fmt.Println("Sum",a+b)
+}
+
+
+package main 
+
+import "fmt"
+
+func main(){
+	var a,b int
+	fmt.Println("Entre two numbers:")
+	fmt.Scan(&a,&b)
+	fmt.Println("Sum", a+b)
+}
+
+
+package main 
+
+import "fmt"
+
+func main() {
+	var a, b int
+	fmt.Println("Entre two numbers:")
+	fmt.Scan(&a, &b)
+	fmt.Println("Sum", a+b)
+}
+
+
+package main 
+
+import "fmt"
+
+func main() {
+	var a, b int
+	fmt.Println("Entre two numbers:")
+	fmt.Scan(&a, &b)
+	fmt.Println("Sum", a+b)
+}
+
+
+package main 
+
+import "fmt"
+
+func main() {
+	var a, b int 
+	fmt.Println("Entre two numbers:")
+	fmt.Scan(&a, &b)
+	fmt.Println("Sum", a+b)
+}
+
+
+package main
+
+import "fmt"
+
+func main() {
+	var a, b int
+	fmt.Println("Entre two numbers:")
+	fmt.Scan(&a, &b)
+	fmt.Println("Sum",a+b)
+}
+
+
+package main 
+
+import "fmt"
+
+func main() {
+	var a, b int
+	fmt.Println("Entre two numbers:")
+	fmt.Scan(&a, &b)
+	fmt.Println("Sum",a+b)
+}
+
+
+package main 
+
+import "fmt"
+
+func main() {
+	var a , b int 
+	fmt.Println("Entre two numbers:")
+	fmt.Scan(&a, &b)
+	fmt.Println("Sum",a+b)
+}
+
+
+package main 
+
+import "fmt"
+
+func main() {
+	var a, b int
+	fmt.Println("Entre two numbers:")
+	fmt.Scan(&a, &b)
+	fmt.Println("Sum",a+b)
+}
+
+
+package main 
+
+import "fmt"
+
+func main() {
+	var a, b int 
+	fmt.Println("Entre two numbers:")
+	fmt.Scan(&a,&b)
+	fmt.Println("Sum"a+b)
+}
+
+
+package main 
+
+import "fmt"
+
+func main() {
+	var a, b int 
+	fmt.Println("Entre two numbers:")
+	fmt.Scan(&a, &b)
+	fmt.Println("Sum",a+b)
+}
+
+package main 
+
+import "fmt"
+
+func main() {
+	var a, b int 
+	fmt.Println("Entre two numbers:")
+	fmt.Scan(&a, &b)
+	fmt.Println("Sum",a+b)
+}
+
+
+package main
+
+import "fmt"
+
+func main() {
+	var a, b int 
+	fmt.Println("Entre two numbers:")
+	fmt.Scan(&a, &b)
+	fmt.Println("Sum", a+b)
+}
+
+
+package main 
+
+import "fmt"
+
+
+func main() {
+	var a , b int
+	fmt.Println("Entre two numbers:")
+	fmt.Scan(&a,&b)
+	fmt.Prinln("Sum",a+b)
+}
+
+
+package main 
+
+import "fmt"
+
+func main() {
+	var a, b int 
+	fmt.Println("Entre two numbers:")
+	fmt.Scan(&a, &b)
+	fmt.Print("Sum",a+b)
+
+}
